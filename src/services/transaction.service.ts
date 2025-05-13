6dadf9204da00653fbe7d454bc8a2c17bda8187f
Rijwan <7rijwan1998@gmail.com>
2025-05-13 23:21:53 +0700

    Booking Success Integration

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -16,7 +16,13 @@ export const transactionApi = apiSlice.injectEndpoints({
                 body: payload,
             })
         }),
+        getDetailTransaction: builder.query({
+            query: (id) => ({
+                url: `/transaction/${id}`,
+                method: 'GET',
+            })
+        }),
     })
 })
 
-export const { useCheckAvailabilityMutation, useTransactionMutation } = transactionApi
+export const { useCheckAvailabilityMutation, useTransactionMutation, useGetDetailTransactionQuery } = transactionApi
