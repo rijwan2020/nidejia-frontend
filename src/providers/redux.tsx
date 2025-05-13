7a46aa060616cb62aae11b65f05baa1fedd37d9e
Rijwan <7rijwan1998@gmail.com>
2025-05-13 20:09:58 +0700

    NextAuth ENV and Provider

--- src/providers/redux.tsx
+++ src/providers/redux.tsx
@@ -2,9 +2,12 @@
 
 import { store } from "@/lib/store";
 import { Provider } from "react-redux";
+import { SessionProvider } from "next-auth/react"
 
 function ReduxProvider({ children }: { children: React.ReactNode }) {
-    return <Provider store={store}>{children}</Provider>
+    return <SessionProvider>
+        <Provider store={store}>{children}</Provider>
+    </SessionProvider>
 }
 
 export default ReduxProvider
