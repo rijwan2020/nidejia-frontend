502369fe6459057df9dbf95b2080781ff738abe4
Rijwan <7rijwan1998@gmail.com>
2025-05-13 20:04:53 +0700

    NextAuth Auth Lib Option

---
+++ next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+    interface Session
+    {
+        user: {
+            id: number;
+            token: string;
+        } & DefaultSession["user"];
+    }
+
+    interface User {
+        token: string;
+    }
+}
